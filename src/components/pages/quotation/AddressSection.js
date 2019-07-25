6f10d94faef6013cf49c4d043f7a44877b56d9b5
Chathura Hettiarachchi <Chathura.h@fidenz.com>
2019-07-25 16:14:42 +0530

    upto scope of work

--- src/components/pages/quotation/AddressSection.js
+++ src/components/pages/quotation/AddressSection.js
@@ -4,27 +4,25 @@ import {
     Grid
 } from "semantic-ui-react";
 
+import './Quotation.css'
+
 function AddressSection() {
     return(
-        <Grid style={{minHeight:'80px', padding: '8px'}}>
+        <Grid style={{minHeight:'80px'}}>
             <Grid.Column width={4}>
-                <Grid.Row>
-                    <Header><b>Customer:</b></Header>
-                    <address style={{fontSize:18, color:'#535353'}}>Client name here<br/>
-                        Address line 1<br/>
-                        Address line 2<br/>
-                        email address of the client</address>
-                </Grid.Row>
+                <Header><b>Customer:</b></Header>
+                <address style={{fontSize:18, color:'#535353'}}>Client name here<br/>
+                    Address line 1<br/>
+                    Address line 2<br/>
+                    email address of the client</address>
             </Grid.Column>
             <Grid.Column width={8}/>
             <Grid.Column width={4} textAlign='right'>
-                <Grid.Row>
-                    <Header><b>From:</b></Header>
-                    <address style={{fontSize:18, color:'#535353'}}>Fidenz Private Limited<br/>
-                        No. 239, Nawala Road,<br/>
-                        Nawala, Sri Lanka<br/>
-                        info@fidenz.com</address>
-                </Grid.Row>
+                <Header><b>From:</b></Header>
+                <address style={{fontSize:18, color:'#535353'}}>Fidenz Private Limited<br/>
+                    No. 239, Nawala Road,<br/>
+                    Nawala, Sri Lanka<br/>
+                    info@fidenz.com</address>
             </Grid.Column>
         </Grid>
     );
--- src/components/pages/quotation/QuotCreateStepTwo.js
+++ src/components/pages/quotation/QuotCreateStepTwo.js
@@ -14,10 +14,11 @@ import QuotationInfo from './QuotationInfo'
 
 import fidenz from '../../../assets/images/fidenz.png'
 import quotation from '../../../assets/images/quotation.png'
+import COUNTRY_OPTIONS from "../../../assets/data/countriesData";
 
 function QuotCreateStepOne() {
     return (
-        <div>
+        <div id='quotation'>
             <Segment style={{padding:'50px'}}>
                 <Grid style={{minHeight:'90px'}}>
                     <Grid.Column width={4}>
@@ -28,8 +29,18 @@ function QuotCreateStepOne() {
                         <Image src={quotation} style={{height:'60px'}} floated='right'/>
                     </Grid.Column>
                 </Grid>
-                <AddressSection/>
-                <QuotationInfo/>
+                <div style={{padding:'8px'}}>
+                    <AddressSection/>
+                    <QuotationInfo/>
+                    <br/>
+                    <Form id='quot_title'>
+                        <Form.TextArea fluid placeholder='Project title' maxLength='100' style={{color: "#3371B1", fontSize:"2em", margin:'20px 0px 20px 0px'}}/>
+                        <hr/>
+                        <Header as='h1' style={{color: "#1579D0"}}>Scope of Work</Header>
+                        <Form.TextArea fluid placeholder='Scope of Work' row={3} style={{marginBottom:'30px'}}/>
+                        <Header as='h1' style={{color: "#1579D0"}}>Financial</Header>
+                    </Form>
+                </div>
             </Segment>
         </div>
     );
